5e30cbbfb9f1c9b765843d473c59bb9207f4590a
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-11-19 13:24:41 -0600

    Update exercise09-javascript.js

--- exercise09-javascript.js
+++ exercise09-javascript.js
@@ -1,8 +1,8 @@
 document.getElementById("column").style.color="#793E91";
 function darkMode(){
-  document.body.classList.toggle('dark-mode');
+document.body.classList.toggle('dark-mode');
 }
-clicks=0;
+let clicks=0;
 function counter(){
- clicks+=1; document.getElementById("output").innerHTML=clicks+" is the total count.";
+ clicks+=1;    document.getElementById("output").innerHTML=clicks+" is the total number of clicks.";
 }
