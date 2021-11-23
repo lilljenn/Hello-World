99fc8f8ad37cec85ed5bae762a1621fdd0d43fac
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-11-22 18:36:11 -0600

    Update Volunteer-javascript.js

--- Volunteer-javascript.js
+++ Volunteer-javascript.js
@@ -12,5 +12,5 @@ function map(){
     center:new google.maps.LatLng(29.403190, -98.616730)
     zoom:5;
       }
-var cantataMap=new google.maps.Map(document.getElementById("googleMap"), cantata);
+var cantataMap=new google.maps.Map(document.getElementById("cantataMap"), cantata);
 }
