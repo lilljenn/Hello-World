c9ad3416e84b81edff8a3ebbb06294424421cdc1
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-11-30 22:25:19 -0600

    Edited Volunteer js

--- Volunteer-javascript.js
+++ Volunteer-javascript.js
@@ -1,13 +1,18 @@
 function applicationSubmit(){
   var name=document.getElementById("name").value;
   var output=document.getElementById("output");
-  var daysAvailable=document.getElementById("daysAvailable").value;
+  var reasons=document.getElementById("reasons").value;
   if (name!=""){
-  if (count!=0){
-if (sunday.click==true){
-alert("message");
-}
-    output.innerHTML="Welcome, "+name+". The days that you are avaliable are "+value;
+    if (count!=0){
+      if (reasons!=""){
+        output.innerHTML="Welcome, "+name+"<br/>"+"The days that you are avaliable are "+value+"<br/>"+"Your reasons for volunteering include "+reasons;
+      }
+      else{
+        output.innerHTML="Please write about your reasons for volunteering.";
+      }
+    }
+    else{
+      output.innerHTML="Please select at least one day of availability.";
     }
   }
   else if (name==""){
@@ -28,12 +33,13 @@ sunday.onclick=function(){
   count++;
   if (count %2 == 1){
     sunday.style.backgroundColor="#ffecb3";
-    if(value.includes("Sunday ")==true){
-    value+="";
+      if(value.includes("Sunday ")==true){
+        value+="";
+      }
+      else{
+        value+="Sunday ";
+      }
     }
-    else{
-    value+="Sunday ";
-  }
   if (count%2 == 0){
     sunday.style.backgroundColor="#ffd966";
   }
@@ -43,10 +49,10 @@ monday.onclick=function(){
   if (count %2 == 1){
     monday.style.backgroundColor="#ffecb3";
     if (value.includes("Monday ")==true){
-    value+="";
+      value+="";
     }
     else{
-    value+="Monday ";
+      value+="Monday ";
     }
   }
   if (count%2 == 0){
@@ -57,11 +63,11 @@ tuesday.onclick=function(){
   count++;
   if (count %2 == 1){
     tuesday.style.backgroundColor="#ffecb3";
-        if (value.includes("Tuesday ")==true){
-    value+="";
+    if (value.includes("Tuesday ")==true){
+      value+="";
     }
     else{
-    value+="Tuesday ";
+      value+="Tuesday ";
     }
   }
   if (count%2 == 0){
@@ -73,10 +79,10 @@ wednesday.onclick=function(){
   if (count %2 == 1){
     wednesday.style.backgroundColor="#ffecb3";
     if (value.includes("Wednesday ")==true){
-    value+="";
+      value+="";
     }
     else{
-    value+="Wednesday ";
+      value+="Wednesday ";
     }
   }
   if (count%2 == 0){
@@ -87,11 +93,11 @@ thursday.onclick=function(){
   count++;
   if (count %2 == 1){
     thursday.style.backgroundColor="#ffecb3";
-        if (value.includes("Thursday ")==true){
-    value+="";
+    if (value.includes("Thursday ")==true){
+      value+="";
     }
     else{
-    value+="Thursday ";
+      value+="Thursday ";
     }
   }
   if (count%2 == 0){
@@ -102,11 +108,11 @@ friday.onclick=function(){
   count++;
   if (count %2 == 1){
     friday.style.backgroundColor="#ffecb3";
-        if (value.includes("Friday ")==true){
-    value+="";
+    if (value.includes("Friday ")==true){
+      value+="";
     }
     else{
-    value+="Friday ";
+      value+="Friday ";
     }
   }
   if (count%2 == 0){
@@ -117,11 +123,11 @@ saturday.onclick=function(){
   count++;
   if (count %2 == 1){
     saturday.style.backgroundColor="#ffecb3";
-        if (value.includes("Saturday ")==true){
-    value+="";
+    if (value.includes("Saturday ")==true){
+      value+="";
     }
     else{
-    value+="Saturday ";
+      value+="Saturday ";
     }
   }
   if (count%2 == 0){
