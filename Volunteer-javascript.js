6351c8e4db2a2f9a5bf176751e1d321aa74d1a52
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-11-30 19:38:25 -0600

    Edited Volunteer js

--- Volunteer-javascript.js
+++ Volunteer-javascript.js
@@ -1,11 +1,19 @@
 function applicationSubmit(){
   var name=document.getElementById("name").value;
+  var output=document.getElementById("output");
+  var daysAvailable=document.getElementById("daysAvailable").value;
   if (name!=""){
-    document.getElementById("output").innerHTML="Welcome, "+name;
+  if (count!=0){
+if (sunday.click==true){
+alert("message");
+}
+    output.innerHTML="Welcome, "+name+". The days that you are avaliable are "+value;
+    }
   }
   else if (name==""){
-    document.getElementById("output").innerHTML="Please enter a name.";
+    output.innerHTML="Please enter a name.";
   }
+  event.preventDefault();
 }
 var count = 0;
 var sunday=document.getElementById("sunday");
@@ -15,10 +23,16 @@ var wednesday=document.getElementById("wednesday");
 var thursday=document.getElementById("thursday");
 var friday=document.getElementById("friday");
 var saturday=document.getElementById("saturday");
+var value="";
 sunday.onclick=function(){
   count++;
   if (count %2 == 1){
     sunday.style.backgroundColor="#ffecb3";
+    if(value.includes("Sunday ")==true){
+    value+="";
+    }
+    else{
+    value+="Sunday ";
   }
   if (count%2 == 0){
     sunday.style.backgroundColor="#ffd966";
@@ -28,6 +42,12 @@ monday.onclick=function(){
   count++;
   if (count %2 == 1){
     monday.style.backgroundColor="#ffecb3";
+    if (value.includes("Monday ")==true){
+    value+="";
+    }
+    else{
+    value+="Monday ";
+    }
   }
   if (count%2 == 0){
     monday.style.backgroundColor="#ffd966";
@@ -37,6 +57,12 @@ tuesday.onclick=function(){
   count++;
   if (count %2 == 1){
     tuesday.style.backgroundColor="#ffecb3";
+        if (value.includes("Tuesday ")==true){
+    value+="";
+    }
+    else{
+    value+="Tuesday ";
+    }
   }
   if (count%2 == 0){
     tuesday.style.backgroundColor="#ffd966";
@@ -46,6 +72,12 @@ wednesday.onclick=function(){
   count++;
   if (count %2 == 1){
     wednesday.style.backgroundColor="#ffecb3";
+    if (value.includes("Wednesday ")==true){
+    value+="";
+    }
+    else{
+    value+="Wednesday ";
+    }
   }
   if (count%2 == 0){
     wednesday.style.backgroundColor="#ffd966";
@@ -55,6 +87,12 @@ thursday.onclick=function(){
   count++;
   if (count %2 == 1){
     thursday.style.backgroundColor="#ffecb3";
+        if (value.includes("Thursday ")==true){
+    value+="";
+    }
+    else{
+    value+="Thursday ";
+    }
   }
   if (count%2 == 0){
     thursday.style.backgroundColor="#ffd966";
@@ -64,6 +102,12 @@ friday.onclick=function(){
   count++;
   if (count %2 == 1){
     friday.style.backgroundColor="#ffecb3";
+        if (value.includes("Friday ")==true){
+    value+="";
+    }
+    else{
+    value+="Friday ";
+    }
   }
   if (count%2 == 0){
     friday.style.backgroundColor="#ffd966";
@@ -73,6 +117,12 @@ saturday.onclick=function(){
   count++;
   if (count %2 == 1){
     saturday.style.backgroundColor="#ffecb3";
+        if (value.includes("Saturday ")==true){
+    value+="";
+    }
+    else{
+    value+="Saturday ";
+    }
   }
   if (count%2 == 0){
     saturday.style.backgroundColor="#ffd966";
